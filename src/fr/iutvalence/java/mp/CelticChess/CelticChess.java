59b83bb3d265ef8f09aa3c83a5c322ed4bf6aabb
DelaplaceVictoria <victoria.delaplace@iut-valence.fr>
2013-09-27 09:57:25 +0200

    UPDATED CelticChess
    Add new public boolean startgame
    Add CelticChess constructor.

--- src/fr/iutvalence/java/mp/CelticChess/CelticChess.java
+++ src/fr/iutvalence/java/mp/CelticChess/CelticChess.java
@@ -6,5 +6,24 @@ package fr.iutvalence.java.mp.CelticChess;
  */
 public class CelticChess
 {
+ /**
+  *  Use all of the parameter to lunch the game
+  */
 
+public boolean startGame;
+
+/** 
+ * @param Sg  Change his value to start/end the game
+ * 
+ */
+
+
+
+public CelticChess (int Sg)
+{
+    /**
+     *  Call this part to lunch a game
+     */
+    this.startGame = true;
+}
 }

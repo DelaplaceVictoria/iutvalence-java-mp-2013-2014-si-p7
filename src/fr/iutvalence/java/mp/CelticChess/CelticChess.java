ab5fa8f218aba85a00d9ee6738b49838b2900a30
sebastienjean <baz.jean@gmail.com>
2013-10-17 07:07:48 +0200

    Reviewed code, added some TODOs

--- src/fr/iutvalence/java/mp/CelticChess/CelticChess.java
+++ src/fr/iutvalence/java/mp/CelticChess/CelticChess.java
@@ -1,55 +1,55 @@
 package fr.iutvalence.java.mp.CelticChess;
+
 /**
  * This class represents a CelticChess's game
+ * 
  * @author delaplvi and dufourlu
- *
+ * 
  */
 public class CelticChess
 {
+    // TODO (fix) rewrite comment
+    /**
+     * Create the board
+     */
+    public Board board;
 
-/**
- *  Create the board
- */
-public Board board;
-/**
- * Used for the player1
- */
-public Player player1;
-/** 
- *  Used for the player2
- */
-public Player player2;
-
-/**
- *  Used to set up the time
- */
-public Chrono chrono;
-
-/** 
- *  Define all of the settings for the game. 
- */
-public CelticChess ()
-{
+    // TODO (fix) rewrite comment
+    /**
+     * Used for the player1
+     */
+    public Player player1;
     
-}   
-/**
- *  Définir ici l'algo pour le déroulement de la partie
- */
-public void startGame()
-{
-    //while
-    //    player1 (dépend si blanc ou noir pour le roi)
-    //    si le roi est dans l'angle ( ou n'existe plus )
-    //    return finpartie
-    // else tour joueur2
-}
+    // TODO (fix) rewrite comment
+    /**
+     * Used for the player2
+     */
+    public Player player2;
 
+    // TODO (fix) rewrite comment
+    /**
+     * Used to set up the time
+     */
+    public Chrono chrono;
 
+    /**
+     * Define all of the settings for the game.
+     */
+    public CelticChess()
+    {
 
+    }
 
-
-
-
-
+    /**
+     * Définir ici l'algo pour le déroulement de la partie
+     */
+    public void startGame()
+    {
+        // while
+        // player1 (dépend si blanc ou noir pour le roi)
+        // si le roi est dans l'angle ( ou n'existe plus )
+        // return finpartie
+        // else tour joueur2
+    }
 
 }
